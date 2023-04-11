98baffbcf9221cedffbeda5e3bd48d3c36a9ceb6
cante-maxence <maxence.cante@edu.univ-fcomte.fr>
2023-04-11 13:49:03 +0200

    Nouvelle page d'accueil + fond degrade movable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,53 @@
 <template>
-    <div class="h-screen w-screen -ml-10 -mt-20 flex items-center">
-        <section class="w-full bg-cover bg-center py-32"
-            style="background-image: url('https://source.unsplash.com/random');">
-            <div class="container mx-auto text-center text-white">
-                <h1 class="text-5xl font-medium mb-6">Welcome to My Agency</h1>
-                <p class="text-xl mb-12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio,
-                    gravida pellentesque urna varius vitae.</p>
-                <button
-                    class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-                    <span
-                        class="relative px-10 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                        Découvrez les galleries
-                    </span>
-                </button>
+    <div class=" w-full -ml-10">
+        <!-- Navbar -->
+
+
+        <!-- Hero section -->
+        <div class="relative  overflow-hidden">
+            <div class="max-w-7xl mx-auto">
+                <div class="relative z-10 pb-8  sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32">
+
+
+
+                    <div class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+                        <div class="sm:text-center lg:text-left">
+                            <h1 class="text-4xl tracking-tight font-extrabold text-white sm:text-5xl md:text-6xl">
+                                <span class="block xl:inline">Votre nouveau tatouage</span>
+                                <span class="block text-zinc-600 xl:inline"> à portée de main.</span>
+                            </h1>
+                            <p
+                                class="mt-3 text-base text-white sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                                Nous sommes une équipe de tatoueurs expérimentés, prêts à vous offrir un travail
+                                personnalisé et de qualité.
+                            </p>
+                            <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                <div class="rounded-md shadow">
+                                    <button
+                                        class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                                        <span
+                                            class="relative px-10 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                            Gallerie Graph
+                                        </span>
+                                    </button>
+                                </div>
+                                <div class="mt-3 sm:mt-0 sm:ml-3">
+                                    <button
+                                        class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                                        <span
+                                            class="relative px-10 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                                            Gallerie Graph
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>
+        </div>
+
+
     </div>
 </template>
+
