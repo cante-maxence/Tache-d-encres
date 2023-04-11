76742ac46ed1a6ee594c5c68fbcc8509bbae467e
cante-maxence <maxence.cante@edu.univ-fcomte.fr>
2023-04-11 15:39:19 +0200

    FAQ eb cours

--- src/views/FAQView.vue
+++ src/views/FAQView.vue
@@ -1,1 +1,64 @@
-<template></template>
+<template>
+    <section class="-ml-10 h-full w-full  sm:py-16 lg:py-24">
+        <div class="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-2xl mx-auto text-center">
+                <h2 class="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">Questions & Réponses</h2>
+                <p class="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-300">Explorez les questions et réponses
+                    courantes a propos du salon.</p>
+            </div>
+
+            <div class="grid grid-cols-1 mt-12 md:mt-20 md:grid-cols-2 gap-y-16 gap-x-20">
+                <div class="flex items-start">
+                    <div class="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-700 rounded-full">
+                        <span class="text-lg font-semibold text-white">?</span>
+                    </div>
+                    <div class="ml-4">
+                        <p class="text-xl font-semibold text-white">Vous êtes situés où ?</p>
+                        <p class="mt-4 text-base text-gray-400">Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                            do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                    </div>
+                </div>
+
+                <div class="flex items-start">
+                    <div class="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-700 rounded-full">
+                        <span class="text-lg font-semibold text-white">?</span>
+                    </div>
+                    <div class="ml-4">
+                        <p class="text-xl font-semibold text-white">Quelle est votre style ?</p>
+                        <p class="mt-4 text-base text-gray-400">Insta et galleries.</p>
+                    </div>
+                </div>
+
+                <div class="flex items-start">
+                    <div class="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-700 rounded-full">
+                        <span class="text-lg font-semibold text-white">?</span>
+                    </div>
+                    <div class="ml-4">
+                        <p class="text-xl font-semibold text-white">Vous tatoué les mineurs ?</p>
+                        <p class="mt-4 text-base text-gray-400">Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                            do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                    </div>
+                </div>
+
+                <div class="flex items-start">
+                    <div class="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-700 rounded-full">
+                        <span class="text-lg font-semibold text-white">?</span>
+                    </div>
+                    <div class="ml-4">
+                        <p class="text-xl font-semibold text-white">Puis-je voir vos précédents tatouages ?</p>
+                        <p class="mt-4 text-base text-gray-400">Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                            do amet sint. Velit officia consequat duis enim velit mollit.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex items-center justify-center mt-12 md:mt-20">
+                <div class="px-8 py-4 text-center bg-gray-800 rounded-full">
+                    <p class="text-gray-50">Tu n'a pas trouvé ton bohneur ? <a href="/contact" title=""
+                            class="text-blue-300 transition-all duration-200 hover:text-blue-400 focus:text-blue-400 hover:underline">Contactez-moi</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
