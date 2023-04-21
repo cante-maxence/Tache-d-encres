f22309552e1da3466d7b280e67da7dfbef61428c
cante-maxence <maxence.cante@edu.univ-fcomte.fr>
2023-04-21 15:40:18 +0200

    Menu mobile a faire

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import ArtisteView from '../views/ArtisteView.vue'
 import SalonView from '../views/SalonView.vue'
 import FAQView from '../views/FAQView.vue'
 import PolitiquesView from '../views/PolitiquesView.vue'
+import testView from '../views/testView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -57,6 +58,11 @@ const router = createRouter({
       path: '/FAQ',
       name: 'FAQ',
       component: FAQView
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: testView
     },
 
     { path: '/:pathMatch(.*)*', component: NotFoundView },
---
+++ src/views/testView.vue
@@ -0,0 +1,4 @@
+<template>
+    <p class="text-2xl pb-8  font-medium">La page que vous cherchez n'existe pas. Elle a
+        peut-être été déplacée ou supprimée.</p>
+</template>
