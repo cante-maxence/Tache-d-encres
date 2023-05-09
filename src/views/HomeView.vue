86346a008ed1211cf206feb94782eb355cc7cd92
cante-maxence <maxence.cante@edu.univ-fcomte.fr>
2023-05-09 14:12:27 +0200

    enfin régler le problême du petit menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class=" w-full -ml-10">
+    <div class=" w-full ">
         <div class="relative  overflow-hidden">
             <div class="max-w-7xl mx-auto">
                 <div class="relative z-10 pb-8  sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32">
